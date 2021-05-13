f1c6660468355da296ea04b44b280811f7ef5595
Tyler Bratt <tyler~bratt@hotmail.com>
2021-05-13 15:35:38 -0400

    can now add form and cancel back to empty state

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -1,21 +1,38 @@
 import React from 'react'
-import "components/appointment/styles.scss"
-import Header from 'components/appointment/Header'
-import Empty from 'components/appointment/Empty'
-import Show from 'components/appointment/Show'
-
+import "./styles.scss"
+import Header from './Header'
+import Empty from './Empty'
+import Show from './Show'
+import Form from './Form'
+import useVisualMode from 'Hooks/useVisualMode'
 
 
 export default function Appointment (props) {
-  const timeSlot = props.interview ? 
-        <Show student={ props.interview && props.interview.student}
-        interviewer={props.interview && props.interview.interviewer}
-        /> : <Empty />
-        
+  const EMPTY = "EMPTY";
+  const SHOW = 'SHOW';
+  const CREATE = 'CREATE';
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-        {timeSlot}
+
+      {mode === EMPTY && <Empty onAdd={()=> transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form 
+          interviewers={[]} 
+          onCancel={()=>transition(EMPTY)}/>
+      )}
     </article>
   )
+
 }
